eae3d9a072c364d4115e0ebdbdfa4ad2a97bb164
Tim Sears <tim@timsears.com>
2020-12-30 15:44:14 -0800

    clean up code

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
   inputs.jupyterWith.url = "github:tweag/jupyterWith";
   inputs.jupyterWith.flake = false;
   
-  outputs = {self, nixpkgs, flake-utils, mach-nix, jupyterWith }:
+  outputs = {self, ... }@inputs : with inputs;
     flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
       let
         
@@ -25,11 +25,6 @@
 
         myPython = mylib.mkPython rec {
           requirements = ''
-            # conda
-            # pip
-            # virtualenv
-            # setuptools
-            #
             jupyterlab
             matplotlib
             numpy
@@ -50,67 +45,9 @@
           '';
         };
           
-        # iPython = jupyter.kernels.iPythonWith {
-        #   name = "mach-nix-jupyter";
-        #   #python3 = myPython.python;
-        #   # packages = myPython.python.pkgs.selectPkgs;
-        # };
-        
-        # jupyterEnvironment = jupyter.jupyterlabWith {
-        #   kernels = [
-        #     iPython
-        #   ];
-        # };
-        
         devShell = myShell;
         defaultPackage = myPython;
         
-
-        # paganini-custom = mylib.mkPython {
-        #   requirements = ''
-        #     paganini==1.3.3
-        #     cvxpy>=1.1.7 
-        #     scs==2.1.1-2 # scs 2.1.2 broken 
-        #   '';
-        # };
-
-        # devShell = pkgs.mkShell {
-        #   #name = "ml-dev";
-        #   buildInputs = with pkgs; [
-        #     python37Packages.conda
-        #     python37Packages.virtualenv
-        #     python37Packages.pip
-        #     python37Packages.setuptools
-        #     python37Packages.numpy
-        #     python37Packages.scikitlearn
-        #     python37Packages.scipy
-        #     python37Packages.matplotlib
-        #     python37Packages.notebook
-        #     # a couple of deep learning libraries
-        #     #python37Packages.tensorflowWithCuda 
-        #     #python37Packages.Keras
-        #     #python37Packages.pytorchWithCuda
-        #     python37Packages.pytorch
-        #     python37Packages.torchvision
-
-        #     # Lets assume we also want to use R, maybe to compare sklearn and R models
-        #     R
-        #     rPackages.mlr
-        #     rPackages.data_table # "_" replaces "."
-        #     rPackages.ggplot2
-            
-        #   ];
-          
-        #   shellHook = ''
-        #   # add non-nix-supported packages here if needed. 
-        #   # virtualenv example...
-        #   # echo "yellowbrick" > requirements.txt
-        #   # virtualenv simpleEnv
-        #   # source simpleEnv/bin/activate
-        #   # pip install -r requirements.txt 
-        #  '';
-        # };
-
       in {
         inherit devShell;
         defaultPackage = myPython;
