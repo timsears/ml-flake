1b92e0f0e2ac9e7efb2694f2a363f371cc04b6a7
Tim Sears <tim@timsears.com>
2021-01-03 14:19:57 -0800

    clean up

--- flake.nix
+++ flake.nix
@@ -3,38 +3,19 @@
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.mach-nix.url = "github:DavHau/mach-nix";
-  inputs.jupyterWith.url = "github:tweag/jupyterWith";
-  inputs.jupyterWith.flake = false;
   
   outputs = {self, ... }@inputs : with inputs;
     flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
       let
         
-        # pkgs = nixpkgs.legacyPackages.${system};
-
         pkgs = import nixpkgs {
           inherit system;
           config = {
             allowUnfree = true;
             allowBroken = true;
-            # allowUnsupportedSystem = true;
           };
         };
         
-        mylib = mach-nix.lib.${system}; # adds mkPython, mkPythonShell, etc. 
-
-        # myPython = mylib.mkPython rec {
-        #   requirements = ''
-        #     jupyterlab
-        #     matplotlib
-        #     numpy
-        #     torch
-        #     scipy
-        #     scikitlearn
-        #     torchvision
-        #   '';          
-        # };
-
         myPython = (pkgs.python37.withPackages (p: with p; [
           pytorchWithCuda
           jupyterlab
@@ -43,6 +24,7 @@
         ])).override (_ : { ignoreCollisions = true; });
 
         myShell = pkgs.mkShell rec {
+
           buildInputs = [
             myPython
             pkgs.conda
@@ -54,11 +36,11 @@
         };
           
         devShell = myShell;
+
         defaultPackage = myPython;
         
       in {
-        inherit devShell;
-        defaultPackage = myPython;
+        inherit devShell defaultPackage;
       }
     );
 }
