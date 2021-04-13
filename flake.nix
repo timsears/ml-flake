0de9b8920ac4a8687274775edc0df1684e5ce4d4
Tim Sears <tim@timsears.com>
2021-04-13 15:43:45 -0700

    mach-nix works but cudaSupport does nothing.

--- flake.nix
+++ flake.nix
@@ -1,4 +1,5 @@
 
+
 {
   description = "Dev shell for machine learning";
   inputs.nixpkgs.url = "nixpkgs-unstable";
@@ -9,33 +10,63 @@
     flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
       let
 
+        machnix = import mach-nix {
+          python = "python38";
+          inherit pkgs;
+        };
+        
         pkgs = import nixpkgs {
           inherit system;
           config = {
             allowUnfree = true;
             allowBroken = true;
-            overlays = final: prev: {
-              python38 = prev.python38.override {
-                packageOverrides = pyfinal: pyprev: {
-                  pytorch-lightning = pyfinal.python3.pkgs.callPackage ./pytorch-lightning { };
-                };
-              };
-            };
+            # overlays = final: prev: {
+            #   python38 = prev.python38.override {
+            #     packageOverrides = pyfinal: pyprev: {
+            #       pytorch-lightning = pyfinal.python3.pkgs.callPackage ./pytorch-lightning { };
+            #     };
+            #   };
+            # };
           };
         };
     
-        myPython = (pkgs.python38.withPackages (p: with p; [
-          jupyterlab
-          ipywidgets
-          matplotlib
-          pyro-ppl
-          #pytorchWithCuda
-          pytorch-bin
-          pytorch-lightning
-          scikitlearn
-          tensorflow-tensorboard_2
-          torchvision
-        ])).override (_ : { ignoreCollisions = true; });
+        #myPython = (pkgs.python38.withPackages (p: with p; [
+        myPython = machnix.mkPython {
+          requirements = '' 
+            pytorch-lightning 
+            jupyterlab
+            ipywidgets
+            matplotlib
+            pyro-ppl
+            #pytorchWithCuda
+            #pytorch-bin
+            #torch
+            pytorch-lightning
+            PyYAML == 5.3.1
+            scikit-learn
+            tensorboard
+            torchvision
+          '' ;
+          providers.torch = "nixpkgs";
+          overridesPost = [(curr: prev: {
+            torch = prev.torch.override {
+              cudaSupport = true;
+            };
+          })];
+          # packagesExtra = [
+          # jupyterlab
+          # ipywidgets
+          # matplotlib
+          # pyro-ppl
+          # #pytorchWithCuda
+          # pytorch-bin
+          # #pytorch-lightning
+          # scikitlearn
+          # tensorflow-tensorboard_2
+          # torchvision
+          # ];
+        };
+    #)).override (_ : { ignoreCollisions = true; });
 
         myShell = pkgs.mkShell rec {
 
@@ -43,9 +74,10 @@
             myPython
             pkgs.conda
           ];
-          
+
+
           shellHook = ''
-            jupyter lab --ip="0.0.0.0" --notebook-dir="~/" --no-browser
+            jupyter notebook --ip="0.0.0.0" --notebook-dir="~/" --no-browser
           '';
         };
           
